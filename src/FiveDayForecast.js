0a4321eb1b35ebc0b8cb1a11a37472390d2805fa
KarenStewart1 <karenstewart1117@gmail.com>
2021-10-29 16:55:59 +0200

    Added weather information

---
+++ src/FiveDayForecast.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./FiveDayForecast.css";
+
+export default function FiveDayForecast() {
+  return <div class="row five-day-forecast" />;
+}
---
+++ src/TodayWeather.js
@@ -0,0 +1,57 @@
+import React from "react";
+import "./TodayWeather.css";
+import axios from "axios";
+
+export default function TodayWeather() {
+  let weatherData = {
+    currentTemp: 18,
+    humidity: 80,
+    windSpeed: 5,
+    weatherDescription: "Sunny",
+  };
+  return (
+    <div className="row today-weather">
+      <div className="row-1 left-side">
+        <div className="col">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/48/sunny.png"
+            alt="weather-icon"
+            className="weather-icon"
+            width="170"
+            height="120"
+          />
+        </div>
+      </div>
+      <div className="row-1 right-side">
+        <div className="col">
+          <h2>
+            <span className="current-temperature">
+              {weatherData.currentTemp}
+            </span>
+            <span className="cf-symbol">
+              <a href="#" className="active">
+                °C{" "}
+              </a>
+              |
+              <a href="#" className="inactive">
+                °F
+              </a>
+            </span>
+          </h2>
+          <ul>
+            <li className="col today-weather-description">
+              {weatherData.weatherDescription}
+            </li>
+            <li>
+              Humidity: <span>{weatherData.humidity}</span>%
+            </li>
+            <li>
+              Wind: <span>{weatherData.windSpeed}</span>
+              <span> m/s</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
