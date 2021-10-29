8119e6b25248fc9ba4a93dda2943b14b2ca035f0
KarenStewart1 <karenstewart1117@gmail.com>
2021-10-29 16:55:36 +0200

    Added Header and Footer

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <small>
+      <a
+        href="https://github.com/KarenStewart1/Weather-App-React"
+        target="_blank"
+        title="Github"
+      >
+        Open-source code
+      </a>
+      by Karen Stewart
+    </small>
+  );
+}
