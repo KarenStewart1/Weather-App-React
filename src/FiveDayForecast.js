585c33dc22df5262d6e96cea0fb87efda9a64edc
KarenStewart1 <karenstewart1117@gmail.com>
2021-10-31 19:52:59 +0100

    added forecast html

--- src/FiveDayForecast.js
+++ src/FiveDayForecast.js
@@ -3,5 +3,30 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./FiveDayForecast.css";
 
 export default function FiveDayForecast() {
-  return <div className="row five-day-forecast" />;
+  let forecastDay = "Monday";
+  let description = "description";
+  let highTemp = Math.round(12);
+  let lowTemp = Math.round(2);
+  let icon = 
+
+  return (
+    <div className="row five-day-forecast">
+      <div className="col-2 daily-forecast">
+        <span className="day">{forecastDay}]</span>
+        <div className="col forecast-description">{description}</div>
+        <div className="col">
+          <img
+            src="images/${forecastDay.weather[0].icon}.png"
+                        width="60"
+            height="70"
+            className="forecast-icons"
+          />
+          <div className="forecast-temperature">
+            {highTemp}° <span className="slash">/</span>
+            {lowTemp}°
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
