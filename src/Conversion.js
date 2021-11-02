637fff406e48c6297c17c0f0fa5807dd0ecccbd2
KarenStewart1 <karenstewart1117@gmail.com>
2021-11-02 18:01:29 +0100

    removing conversion

--- src/Conversion.js
+++ src/Conversion.js
@@ -1,39 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Conversion.css";
 
 export default function Conversion(props) {
-  let [unit, setUnit] = useState("celsius");
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-  if (unit === "celsius") {
-    return (
-      <h2>
-        <span className="current-temperature">{props.celsiusTemp}</span>
-        <span className="cf-symbol">
-          <a href="/" className="active" onClick={showCelsius}>
-            °C{" "}
-          </a>
-        </span>
-      </h2>
-    );
-  } else {
-    let fahrenheitTemp = Math.round((props.celsiusTemp * 9) / 5 + 32);
-    return (
-      <h2>
-        <span className="current-temperature">{fahrenheitTemp}</span>
-        <span className="cf-symbol">
-          <a href="/" className="inactive" onClick={showCelsius}>
-            °C{" "}
-          </a>
-        </span>
-      </h2>
-    );
-  }
+  return (
+    <h2>
+      <span className="current-temperature">{props.celsiusTemp}</span>
+      <span className="cf-symbol">
+        <a href="/" className="active">
+          °C{" "}
+        </a>
+      </span>
+    </h2>
+  );
 }
